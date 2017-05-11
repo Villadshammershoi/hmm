a1096cefb54fb5e03e624cb199e4b03d8bd374af
Villadshammershoi <villadshammershoei@hotmail.com>
2017-05-11 12:35:30 +0200

    Added: Register validation
    Added: Register validation, angular directive and more html.

---
+++ hmm/Scripts/Angular/Default/Directives/passwordMatch.js
@@ -0,0 +1,65 @@
+﻿//(function (angular) {
+//    var app = angular.module('offerend')
+//    app.directive("noMatch", function ($timeout) {
+//        return {
+//            restrict: "A",
+//            require: "ngModel",
+
+//            link: function (scope, element, attributes, ngModel) {
+//                ngModel.$validators.noMatch = function (modelValue) {
+//                    return attributes.noMatch !== modelValue;
+//                };
+//            }
+//        };
+//    });
+//});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//angular.module('UserValidation', []).directive('validPasswordC', function () {
+//    return {
+//        require: 'ngModel',
+//        link: function (scope, elm, attrs, ctrl) {
+//            ctrl.$parsers.unshift(function (viewValue, $scope) {
+//                var noMatch = viewValue != scope.myForm.password.$viewValue
+//                ctrl.$setValidity('noMatch', !noMatch)
+//            })
+//        }
+//    }
+//})
+
+
+
+//(function (angular) {
+//    var app = angular.module('offerend');
+//    app.directive('validPasswordC', [function () {
+//        return {
+//            require: 'ngModel',
+//            link: function (scope, elm, attrs, ctrl) {
+//                ctrl.$parsers.unshift(function (viewValue, $scope) {
+//                    var noMatch = viewValue != scope.offerendRegisterForm.password.$viewValue
+//                    ctrl.$setValidity('noMatch', !noMatch)
+//                })
+//            }
+//        }
+//    }]);
+//})(angular);
+
+
+
--- hmm/Scripts/Angular/Login/Controllers/LoginRegisterController.js
+++ hmm/Scripts/Angular/Login/Controllers/LoginRegisterController.js
@@ -1,48 +1,60 @@
 ﻿(function (angular) {
     var app = angular.module('offerend.login');
     app.controller('LoginRegisterController', ['$scope', '$uibModal', '$http', '$location', function ($scope, $uibModal, $http, $location) {
-        $scope.createAccount = { username: "", password: "", repeatPassword: "", email: "", repeatEmail: "", validationMessage: "", registerSuccessMessage: ""};
-
-
-
-        $scope.validatePassword = function () {
-            if ($scope.createAccount.password != $scope.createAccount.repeatPassword) {
-                $scope.createAccount.validationMessage = "Password and Confirm Password don't match.";
-            }
-        };
-
-        $scope.validateUsername = function () {
-            if ($scope.createAccount.username.length < 5) {
-                $scope.createAccount.validationMessage = "Username is too short";
-            }
-        }
-
+        $scope.createAccount = { username: "", password: "", confirmPassword: "", email: "", confirmEmail: "", validationMessage: "", registerSuccessMessage: "" };
+        var $ctrl = this;
+        $ctrl.animationsEnabled = true;
 
         //Create Account
-        $scope.createAccount = function () {
-            $http.post('../Account/Register?username=' + $scope.createAccount.username + '&password=' + $scope.createAccount.password + '&repeatPassword=' + $scope.createAccount.repeatPassword + '&email=' + $scope.createAccount.email + '&repeatEmail=' + $scope.createAccount.repeatEmail)
-                .then(function (data, headers, status) {
-                if (!!data && data.Success == true) {
-                    $scope.createAccount.registerSuccessMessage = "Account successfully created!";
-                }
-            });
+        $scope.createAccount = function (isValid, size, parentSelector) {
+            if (isValid) {
+                var modalInstance = $uibModal.open({
+                    animation: $ctrl.animationsEnabled,
+                    templateUrl: 'ModalContent.html',
+                    controller: 'ModalInstanceController',
+                    controllerAs: '$ctrl',
+                });
+
+                $http.post('../Account/Register?username=' + $scope.createAccount.username + '&password=' + $scope.createAccount.password + '&repeatPassword=' + $scope.createAccount.repeatPassword + '&email=' + $scope.createAccount.email + '&repeatEmail=' + $scope.createAccount.repeatEmail)
+                    .then(function (data, headers, status) {
+                        if (!!data && data.Success == true) {   
+                            $scope.createAccount.registerSuccessMessage = "Account successfully created!";
+                            alert("you did it, your form is working!");
+                        }
+
+                    });
+            }
         };
+
+        //$scope.checkPasswords = function () {
+        //    if ($scope.offerendRegisterForm.$invalid && $scope.createAccount.password != $scope.createAccount.repeatPassword) {
+        //        $scope.createAccount.validationMessage = "Your password aint matching!!!";
+        //    }
+        //};
 
 
         $scope.form = 1;
-        var $ctrl = this;
 
-        $ctrl.animationsEnabled = true;
 
-        $ctrl.open = function (size, parentSelector) {
-            var modalInstance = $uibModal.open({
-                animation: $ctrl.animationsEnabled,
-                templateUrl: 'myModalContent.html',
-                controller: 'ModalInstanceController',
-                controllerAs: '$ctrl',
-            });
+
+
+
+    }])
+
+
+
+    app.directive("matcher", function ($timeout) {
+        return {
+            restrict: "A",
+            require: "ngModel",
+
+            link: function (scope, element, attributes, ngModel) {
+                ngModel.$validators.matcher = function (modelValue) {
+                    return attributes.matcher === modelValue;
+                };
+            }
         };
-    }]);
+    });
 
     app.controller('ModalInstanceController', ['$uibModalInstance', function ($uibModalInstance) {
         var $ctrl = this;
